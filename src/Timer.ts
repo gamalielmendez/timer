112fcee8777e0ff7cbf03723a9a77f7a376b0aee
gamalielmendez <diana22288@gmail.com>
2021-01-08 18:46:25 -0600

    first commit

---
+++ src/Timer.ts
@@ -0,0 +1,46 @@
+export default class Timer{
+
+    private Interval:number=1000
+    private callBack:any=null
+    private myThread:any=null
+    private enabled:boolean=false
+
+    constructor(Interval:number=1000,callBack:any=null){
+        this.Interval=Interval
+        this.callBack=callBack
+    }
+
+    start(){
+        this.myThread=setTimeout(this.Tick,this.Interval,this)
+        this.enabled=false
+    }
+
+    stop(){
+        clearTimeout(this.myThread)
+        this.enabled=false
+    }
+
+    setCallback(callBack:any=null){
+        this.callBack=callBack
+    }
+
+    setInterval(Interval:number=1000){
+        this.Interval=Interval
+    }
+
+    isrunning(){
+        return this.enabled 
+    }
+
+    async Tick(_this:any){
+        
+        if(_this.callBack){
+            await _this.callBack(_this.Interval) 
+        }  
+
+        //se reinicia el hilo
+        _this.start() 
+
+    }
+
+}
